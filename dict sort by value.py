3b5c4d3b27c8063e14d44fd80ffb28c81e5513fd
greedyx <wangtingjun2018@gmail.com>
2021-08-10 10:40:19 -0700

    Add something about dict sort by values

--- dict sort by value.py
+++ dict sort by value.py
@@ -11,3 +11,7 @@ print(c)
 d = sorted(x.items(), key=lambda item: item[0])
 print(d)
 # [(0, (0, 100)), (1, (2, 3)), (2, (1, -1)), (3, (4, 5)), (4, (3, 1))]
+
+c = sorted(x.items(), key=lambda item: item[1][0],reverse=True)
+print(c)
+# [(3, (4, 5)), (4, (3, 1)), (1, (2, 3)), (2, (1, -1)), (0, (0, 100))]
